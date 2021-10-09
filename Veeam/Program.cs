8437d68dd0fc54657dee46d15bd5f0fab7181767
nikita.elanskiy <nikita.elanskiy@maximumtest.ru>
2021-10-09 20:53:09 +0300

    delete comments

--- Veeam/Program.cs
+++ Veeam/Program.cs
@@ -7,30 +7,6 @@ namespace Veeam
 {
     class Program
     {
-        /*
-         Создайте свой иммутабельный (immutable) тип. При изменении какого-либо из полей
-            создается новый объект. В качестве примера можете взять тип String.
-         */
-
-        /*Попробуйте симитировать наследование и переопределение виртуального метода в
-структурах.
-          class Base
- {
- public virtual string GetInfo()
- {
- return "Base class";
- }
- }
- class A : Base
- {
- public override string GetInfo()
- {
- return "A class";
- }
- }
-…
- Base b = new A()
-         */
         static void Main(string[] args)
         {
             Base b = new A();
